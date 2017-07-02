69de4b4a6c0419dba16fd081606761574826b105
GenericEarthling <sharon.tender@icloud.com>
2017-07-02 17:10:42 -0400

    initial files uploaded

---
+++ src/filesearch/FileSearch.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filesearch;
+
+/**
+ *
+ * @author Tender
+ */
+public class FileSearch {
+
+    /**
+     * @param args the command line arguments
+     */
+	public static void main ( String[] args ) {
+		GUIFrame frame = new GUIFrame();
+		frame.setVisible(true);
+	}
+    
+}
